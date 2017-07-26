63a59cb05ff805cc5a7fde1745f4443e7c3e5721
Andretti77 <pettian77@gmail.com>
2017-07-25 21:17:08 -0600

    Restructured some things

--- src/bot.py
+++ src/bot.py
@@ -1,41 +1,56 @@
 import discord 
+from discord import Game
 from discord.ext.commands import Bot
 from urllib.request import urlopen 
 import requests
 from bs4 import BeautifulSoup
 import asyncio as asyncio
 
-bot = Bot(command_prefix = "~")
-#client = discord.Client()
+#bot = Bot(command_prefix = "~")
+client = discord.Client()
 
 
-@bot.event
+@client.event
 async def on_ready():
         print("Logged in")
-        #client.change_presence(game=discord.Game(name="WIP"))
+        await client.change_presence(game=Game(name="Under Construction"))
+
         
-@bot.command()
-async def hello(*args):
-        return await bot.say("Hello!")
+#@bot.command()
+async def hello(message):
+       return await client.send_message(message.channel,"Hello!")
 
 
-@bot.command()
-async def goodbye(*args):
-        return await bot.say("Goodbye!")
+#@bot.command()
+async def goodbye(message):
+        return await client.send_message(message.channel,"Goodbye!")
 
-@bot.command()
-async def youtube(*args):
+
+#@bot.command()
+async def youtube(message):
         results = []
-        search_terms = '+'.join(args)
+        temp = message.content.replace("~youtube ","")
+        search_terms = '+'.join(temp)
         url = "https://www.youtube.com/results?search_query=" + search_terms
         url_o = urlopen(url)
         html = url_o.read()
         s = BeautifulSoup(html, "lxml")
         for vids in s.find_all(attrs= {'class': 'yt-uix-tile-link'},limit = 1):
                 results.append('https://www.youtube.com' + vids['href'])
-                
-       
-        return await bot.say(results[0])
+        return await client.send_message(message.channel, results[0])
 
 
-bot.run("MzM3OTczMjM1NTAzMjAyMzA1.DFOptQ.lSWcXTTnDkY4Igoua0zJNXrX5YU")
+@client.event
+async def on_message(message):
+
+        if message.content.startswith("~hello"):
+               await hello(message)
+        elif message.content.startswith("~goodbye"):
+                await goodbye(message)
+        elif message.content.startswith("~youtube"):
+                await youtube(message) 
+
+
+
+
+client.run("MzM3OTczMjM1NTAzMjAyMzA1.DFOptQ.lSWcXTTnDkY4Igoua0zJNXrX5YU")
